0998f3912b58b782ab70959d1755ad0ea16360a9
Leandro Stoew <leandrojustino83@hotmail.com>
2024-06-07 18:47:04 -0300

    Projeto Dockerfile Nginx

--- git_push_devops.sh
+++ git_push_devops.sh
@@ -12,8 +12,17 @@ git add .
 # Realiza o commit das alterações
 git commit -m "$commit_message"
 
-# Envia as alterações para o repositório remoto
-git push origin main
+# Faz o pull das alterações remotas com rebase
+git pull origin main --rebase
 
-echo "Alterações enviadas para o repositório remoto com sucesso!"
+# Verifica se houve conflitos após o pull
+if [ $? -eq 0 ]; then
+    # Se não houver conflitos, continua com o push
+    # Envia as alterações para o repositório remoto
+    git push origin main
+    echo "Alterações enviadas para o repositório remoto com sucesso!"
+else
+    # Se houver conflitos, exibe uma mensagem de erro e aborta o push
+    echo "Erro: Houve conflitos ao puxar as alterações remotas. Por favor, resolva os conflitos e tente novamente."
+fi
 
